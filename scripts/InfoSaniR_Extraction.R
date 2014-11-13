edf33a8d49deb709b6d9c51cb58873de25572c8d
belell <bertrand.lell@gmail.com>
2014-11-13 19:45:20 +0100

    Update InfoSaniR_Extraction.R
    Format changes according to this styleguide:
    http://r-pkgs.had.co.nz/style.html

--- scripts/InfoSaniR_Extraction.R
+++ scripts/InfoSaniR_Extraction.R
@@ -1,7 +1,7 @@
 ##########################################################################################
 ################Extraction des données d'une année de tous les centres sanitaires#########
 ##########################################################################################
-InfoSaniR_extract<-function(directory,health){
+InfoSaniR_extract <- function(directory,health) {
   
   #Folder configuration
   #####################
@@ -71,14 +71,14 @@ InfoSaniR_extract<-function(directory,health){
   if (!is.element(health,c('patho','accouch1','accouh2','malnu','vac'))){stop("l'argument health mal spécifié!")}
   
   #Extraction de la liste fichiers InfoSani et la date
-  FileCenter<-list.files(file.path(getwd(),'Data',directory))
-  year<-str_extract(FileCenter, "20[0-9]+") 
+  FileCenter <- list.files(file.path(getwd(),'Data', directory))
+  year <- str_extract(FileCenter, "20[0-9]+") 
   
   #Récupération des départements
-  nameDept<-str_sub(FileCenter, start=1,end=str_locate(FileCenter, "_")[,"start"]-1)
+  nameDept <- str_sub(FileCenter, start=1,end=str_locate(FileCenter, "_")[,"start"]-1)
   #Définition Département
-  nameDept2<- rep("Abanga-Bine", length(nameDept)) 
-  nameDept2[str_detect(nameDept, "OL")] <-"Ogouee-Lacs"
+  nameDept2 <- rep("Abanga-Bine", length(nameDept)) 
+  nameDept2[str_detect(nameDept, "OL")] <- "Ogouee-Lacs"
   
   
   #Récupération de type strucutres
@@ -88,15 +88,14 @@ InfoSaniR_extract<-function(directory,health){
       end = str_locate_all(FileCenter, "_")[[i]][2,"start"][[1]]-1)}
   #Définition les types d'établissements de santé
   Centertype <- rep("Autre", length(nametypestruc)) 
-  Centertype[str_detect(nametypestruc, "Disp")] <-"Dispensaire"
-  Centertype[str_detect(nametypestruc, "Inf")] <-"Infirmerie"
-  Centertype[str_detect(nametypestruc, "CS")] <-"Centre de Sante"
-  Centertype[str_detect(nametypestruc, "CM")] <-"Centre Medical"
-  Centertype[str_detect(nametypestruc, "Hop")] <-"Hopital"
+  Centertype[str_detect(nametypestruc, "Disp")] <- "Dispensaire"
+  Centertype[str_detect(nametypestruc, "Inf")] <- "Infirmerie"
+  Centertype[str_detect(nametypestruc, "CS")] <- "Centre de Sante"
+  Centertype[str_detect(nametypestruc, "CM")] <- "Centre Medical"
+  Centertype[str_detect(nametypestruc, "Hop")] <- "Hopital"
   
   #Vérification des fichiers dans le fichier
   if (length(FileCenter)==0){stop("Dossier data vide!")}
-  
   
   #Vérification des fichiers Infosani (formats, structure du nom)
   #Structure du nom:Deptement_TypedeStructure_Nom_Annee.xls
@@ -108,19 +107,19 @@ InfoSaniR_extract<-function(directory,health){
                                              {stop("Nom du fichier InfoSani mal structuré!")}}}
   
   #Récupération des noms de structures
-  namestruc<-c()
+  namestruc <- c()
   for (i in 1:length(FileCenter)){
     namestruc[i]<-str_sub(FileCenter[i], start =str_locate_all(FileCenter, "_")[[i]][2,"start"][[1]]+1,
       end = str_locate_all(FileCenter, "_")[[i]][3,"start"][[1]]-1)}
   
   #Définition des soins médicaux (consultations ou hospitalisations)
-  soin<-rep("consultation",length(FileCenter))
+  soin <- rep("consultation",length(FileCenter))
   soin[str_detect(FileCenter, "Hosp")] <- "hospitalisation"
   
   #Augmentation de la mémoire allouée à R
   options(java.parameters = "-Xmx2048m") 
   ##########################################################################################################
-  if (health=='patho'){
+  if (health == 'patho'){
   
   ##Extraction des données de pathologies
   #Extraction des données de tous les établissements de Santé
@@ -130,15 +129,15 @@ InfoSaniR_extract<-function(directory,health){
     indextab$enrow <-c(50,65,76,121,126,132,155,178,207,220,235,278,293,319,339,347,365,369,370,374,392,431,439,449)
     indextab$coldeas<-c(2,2,2,2,2,4,2,2,2,2,2,2,2,2,2,2,2,4,2,4,2,2,2,2)
     
-    tab0<-vector("list",length(FileCenter))
-    tab1<-vector("list",length(sheetlist)*dim(indextab)[1])
+    tab0 <- vector("list",length(FileCenter))
+    tab1 <- vector("list",length(sheetlist)*dim(indextab)[1])
     start <- Sys.time()
     
     for (k in 1:length(FileCenter)){
       cat("...Extraction des données:",namestruc[k], "\n")
       
       for (i in 1:12){
-        sheet<- read.xlsx(file.path(getwd(),'Data',directory,FileCenter[k]),sheetIndex=sheetlist[i], startRow=556,endRow=1004,
+        sheet<- read.xlsx(file.path(getwd(),'Data', directory, FileCenter[k]), sheetIndex = sheetlist[i], startRow=556,endRow=1004,
           colIndex=c(1:27),header=FALSE)
         for (j in 1:24){
           tab1[[((i-1)*24+j)]]<-sheet[c(indextab[j,1]:indextab[j,2]),c(1,indextab[j,3],8:27)] 
@@ -146,7 +145,6 @@ InfoSaniR_extract<-function(directory,health){
             "0_DCD_M","0_DCD_F", "1_CAS_M", "1_CAS_F", "1_DCD_M","1_DCD_F",
             "5_CAS_M", "5_CAS_F", "5_DCD_M","5_DCD_F", "15_CAS_M", "15_CAS_F",
             "15_DCD_M","15_DCD_F", "50_CAS_M", "50_CAS_F", "50_DCD_M","50_DCD_F")
-          
         }
         rmtm <- round((length(FileCenter)*12 - (k-1)*12 - i)*(difftime(Sys.time(),start,units="mins"))/((k-1)*12 + i),1)
         cat("... Extraction des données du mois: ", month.name[i], "...estimation du temps restant:", rmtm, "minutes \n")      
@@ -158,29 +156,29 @@ InfoSaniR_extract<-function(directory,health){
         ,"Opthalmo","ORL","Stomato","Gyn","Perinat","Autres"),c(50,12,4,72,20,22,10,12,36,12,19,17,5,20,15,32,5,7))
       tab11$Mois<-rep(month.name,each=370)
       
-      tab12<- melt(tab11, id.vars=c("Code_Maladie","Code_Domaine","Domaine","Maladies","Mois"))
-      tab12$Age<- str_extract(tab12$variable, "[0-5]{1,2}")
+      tab12 <- melt(tab11, id.vars=c("Code_Maladie","Code_Domaine","Domaine","Maladies","Mois"))
+      tab12$Age <- str_extract(tab12$variable, "[0-5]{1,2}")
       tab12$Statut_vital <- str_extract(tab12$variable, "[A-S]{3}")
-      tab12$Sexe<- str_extract(tab12$variable, "[M,F]")
-      tab12$Annee<-year[k]
-      tab12$Nom<-namestruc[k]
-      tab12$Departement<-nameDept2[k]
-      tab12$Etablissement<-Centertype[k]
-      tab12$Soin<-soin[k]
-      tab12<-tab12[,c(13,14,12,15,11,5,2,3,1,4,9,10,8,7)]
-      tab0[[k]]<-tab12
+      tab12$Sexe <- str_extract(tab12$variable, "[M,F]")
+      tab12$Annee <- year[k]
+      tab12$Nom <- namestruc[k]
+      tab12$Departement <- nameDept2[k]
+      tab12$Etablissement <- Centertype[k]
+      tab12$Soin <- soin[k]
+      tab12 <- tab12[,c(13,14,12,15,11,5,2,3,1,4,9,10,8,7)]
+      tab0[[k]] <- tab12
       cat("Fin d'extraction des données!\n") 
     }
-    tabfinal<-do.call(rbind,tab0)
-    tabfinal1<-tabfinal[!is.na(tabfinal$value),]
+    tabfinal <- do.call(rbind,tab0)
+    tabfinal1 <- tabfinal[!is.na(tabfinal$value),]
     
     #Exportation des donées vers une table Excel/RData
     ##################################################
-    tabfinal2<-tabfinal1
-    tabfinal2$Age<-as.factor(tabfinal2$Age)
-    tabfinal2$Maladies<-as.character(tabfinal2$Maladies)
-    levels(tabfinal2$Age)<-c("0-11 mois","1-4 ans","15-49 ans","5-14 ans",">49 ans")
-    names(tabfinal2)[14]<-"Effectif"
+    tabfinal2 <- tabfinal1
+    tabfinal2$Age <- as.factor(tabfinal2$Age)
+    tabfinal2$Maladies <- as.character(tabfinal2$Maladies)
+    levels(tabfinal2$Age) <- c("0-11 mois","1-4 ans","15-49 ans","5-14 ans",">49 ans")
+    names(tabfinal2)[14] <- "Effectif"
     tabfinal3<-tabfinal2
     tabfinal3$Maladies<-iconv(tabfinal3$Maladies,from="UTF-8",to="WINDOWS-1252")#Encodage de la variable 
     write.xlsx2(tabfinal3,file=paste(paste("Exports\\Data_patho",year[1],sep=""),"xlsx",sep="."),row.names=F)
